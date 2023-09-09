76441c7826e462a2bd662bb0c5030daedd9e4f70
Moses Hunsu <hunsumoses7@gmail.com>
2023-09-09 11:01:57 +0100

    "Modified github_file_url"

--- src/main/java/com/hngx/hngxstageone/service/impl/ResponseServiceImpl.java
+++ src/main/java/com/hngx/hngxstageone/service/impl/ResponseServiceImpl.java
@@ -23,7 +23,7 @@ public class ResponseServiceImpl implements ResponseService {
                 .utc_time(now.toString().substring(0, 19) + "Z")
                 .track(track)
                 .github_file_url("https://github.com/moseshunsu/hngx-stage-one/blob/main/src/main/java/com/hngx/hngxstageone/HngxStageOneApplication.java")
-                .github_repo_url("https://github.com/moseshunsu/HnGx-stage-one")
+                .github_repo_url("https://github.com/moseshunsu/hngx-stage-one")
                 .status_code(200)
                 .build();
     }
